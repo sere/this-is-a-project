4cdc56aa5d47412efba9ae3e5106ee7698ad0e6f
Arianna Avanzini <avanzini.arianna@gmail.com>
2013-03-26 01:34:47 +0100

    Counter.py: protect count with a lock.
    The lock forces mutual exclusion while accessing the internal count
    attribute of the Counter class.

--- Counter.py
+++ Counter.py
@@ -2,20 +2,27 @@
 #
 # Counter class which handles generation of unique identifiers for resources
 #
+import thread
 
 class Counter:
 
     count = 0
+    lock = None
 
     def __init__(self, init=0):
         self.count = init
+        self.lock = thread.allocate_lock()
 
     def get(self):
+        self.lock.acquire()
         self.count = self.count + 1
+        self.lock.release()
         return self.count
 
     def put(self):
+        self.lock.acquire()
         self.count = self.count - 1
+        self.lock.release()
         return self.count
 
 # vim: set et sts=4 sw=4:
