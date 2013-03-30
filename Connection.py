567571aca5bb90dcf0c945972bbccd537e0fdff5
Arianna Avanzini <avanzini.arianna@gmail.com>
2013-03-30 01:15:43 +0100

    Connection.py: let class names be saved inside a Connection object.
    This further prepares the code to the future introduction of the
    sqlalchemy magic. The patch also introduces a getter method for
    class names. Class names will be very useful, as we will need them
    to know which mapped class we'll have to query when retrieving
    objects.

--- Connection.py
+++ Connection.py
@@ -7,13 +7,21 @@ class Connection:
 
     id1 = None
     id2 = None
+    class1 = None
+    class2 = None
 
-    def __init__(self, id1, id2):
+    def __init__(self, id1, id2, class1, class2):
         assert(id1 != None and id2 != None)
+        assert(class1 != None and class2 != None)
         self.id1 = id1
         self.id2 = id2
+        self.class1 = class1
+        self.class2 = class2
 
-    def get(self):
+    def get_id(self):
         return (self.id1, self.id2)
+
+    def get_class(self):
+        return (self.class1, self.class2)
 
 # vim: set et sts=4 sw=4:
